4920e7caf2f0303c7a26f7a049b0f1617d236bfe
lschpmn <luis@lmc-productions.com>
2015-03-15 16:45:03 -0500

    Added address panel, as well as some style changes

--- code/resources/angular/files/checkout/checkoutCtrl.js
+++ code/resources/angular/files/checkout/checkoutCtrl.js
@@ -1,12 +1,10 @@
 'use strict';
 
 snow.controller('checkoutCtrl',['userFactory','$http','$scope','$window',function(userFactory,$http,$scope,$window){
-    $scope.total = 0;
     refreshOrders();
 
 
     $scope.remove = function(item){
-        console.log(item);
         $http.delete('/api/order/' + item.orderID).then(function(){
             $scope.$emit('updateCart');
             refreshOrders();
@@ -19,7 +17,12 @@ snow.controller('checkoutCtrl',['userFactory','$http','$scope','$window',functio
 
     function refreshOrders(){
         userFactory.getUser().then(function(success){
+            $scope.total = 0;
             $scope.shoppingItems = success.data.orders;
+
+            if($scope.address === undefined){//loads up shipping address, and allow them to change it
+                $scope.address = success.data.addresses[0];
+            }
 
             $scope.shoppingItems.forEach(function(item){
                 $scope.total += item.items.itemPrice * item.itemQTY;
