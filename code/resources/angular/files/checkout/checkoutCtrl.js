d2804b481ffd9e22a49a9677d8defdd1e48d82cc
lschpmn <luis@lmc-productions.com>
2015-03-15 16:21:47 -0500

    Can now remove items from shopping cart in checkout page

--- code/resources/angular/files/checkout/checkoutCtrl.js
+++ code/resources/angular/files/checkout/checkoutCtrl.js
@@ -2,21 +2,28 @@
 
 snow.controller('checkoutCtrl',['userFactory','$http','$scope','$window',function(userFactory,$http,$scope,$window){
     $scope.total = 0;
+    refreshOrders();
 
-    userFactory.getUser().then(function(success){
-        $scope.shoppingItems = success.data.orders;
 
-        $scope.shoppingItems.forEach(function(item){
-            $scope.total += item.items.itemPrice * item.itemQTY;
+    $scope.remove = function(item){
+        console.log(item);
+        $http.delete('/api/order/' + item.orderID).then(function(){
+            $scope.$emit('updateCart');
+            refreshOrders();
         });
-    });
+    };
 
     $scope.buy = function(){
-        if($scope.shoppingItems.some(function(item){return item.get})){
-            console.log('selected');
-            //TODO: Purchase item
-        }else{
-            $window.alert('Please select at least 1 item to buy');
-        }
+
+    };
+
+    function refreshOrders(){
+        userFactory.getUser().then(function(success){
+            $scope.shoppingItems = success.data.orders;
+
+            $scope.shoppingItems.forEach(function(item){
+                $scope.total += item.items.itemPrice * item.itemQTY;
+            });
+        });
     }
 }]);
