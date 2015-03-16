ddf6f9483cf83274db39d851c8d4f6e7b91e101c
lschpmn <luis@lmc-productions.com>
2015-03-15 19:54:51 -0500

    Purchase page now shows success or failure message depending on response from portal

--- code/resources/angular/files/purchased/purchasedCtrl.js
+++ code/resources/angular/files/purchased/purchasedCtrl.js
@@ -1,5 +1,11 @@
 'use strict';
 
-snow.controller('purchasedCtrl',[function(){
+snow.controller('purchasedCtrl',['$location','$scope',function($location,$scope){
+    console.log($location.search());
+    var status = $location.search().StatusCode;
+    $location.search({});
 
+    if(status === 'A'){
+        $scope.success = true;
+    }
 }]);
