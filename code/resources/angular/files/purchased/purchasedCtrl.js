a91cca2be5312d55957e4d29d2731a2a30251a43
lschpmn <luis@lmc-productions.com>
2015-03-15 20:06:45 -0500

    On successful payment, all orders belonging to user are removed from database

--- code/resources/angular/files/purchased/purchasedCtrl.js
+++ code/resources/angular/files/purchased/purchasedCtrl.js
@@ -1,11 +1,22 @@
 'use strict';
 
-snow.controller('purchasedCtrl',['$location','$scope',function($location,$scope){
-    console.log($location.search());
+snow.controller('purchasedCtrl',['$http','$location','$q','$scope','userFactory',function($http,$location,$q,$scope,userFactory){
     var status = $location.search().StatusCode;
     $location.search({});
 
     if(status === 'A'){
         $scope.success = true;
+        userFactory.getUser().then(function(success){
+            var orders = success.data.orders;
+            var promises = [];
+
+            orders.forEach(function(order){
+                promises.push($http.delete('/api/order/' + order.orderID));
+            });
+
+            $q.all(promises).then(function(){
+                $scope.$emit('updateCart');
+            });
+        });
     }
 }]);
